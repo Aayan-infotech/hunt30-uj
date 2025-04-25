c3100f4058ed1927c07f0b2ce089ce8ac936d5cd
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-04-25 15:29:08 +0530

    add change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+// import Navbar from './components/navbar/Navbar';
 // import SignUp from './components/signup/SignUp';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Response from './components/response/Response';
@@ -9,6 +9,8 @@ import TermsAndConditions from'./component/TermsCondition'
 import AboutUs from'./component/AboutUs'
 import Home from './components/Home/Home';
 import Detail from './components/Detail/detail';
+import CotexPrivecyPolicy from './component/coyotex/CoyotexPrivacyPolicy';
+import CoyotexTermsAndConditions from './component/coyotex/CotexTermsAndConditions';
 
 
 function App() {
@@ -23,6 +25,8 @@ function App() {
             <Route path="/termCondition" element={<TermsAndConditions />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/booking" element={<Detail />} />
+            <Route path="/coyotex/terms-and-conditions" element={<CoyotexTermsAndConditions />} />
+            <Route path="/coyotex/privacy-policy" element={<CotexPrivecyPolicy />} />
             {/* <Route path="/home" element={<Home />} /> */}
             <Route path="/" element={<Home />} />
           </Routes>
